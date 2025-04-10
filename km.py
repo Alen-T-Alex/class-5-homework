85f7d1f9862ceee6fb9b4c9cb0938d2b18dcf089
Alen-T-Alex <140389582+Alen-T-Alex@users.noreply.github.com>
2025-04-11 00:52:47 +0300

    Add files via upload

---
+++ km.py
@@ -0,0 +1,4 @@
+print("kilometers to miles converter")
+km=float(input("how many kilometers did you travel? "))
+miles=(km*float(0.62137))
+print(f"if you traveled {km} kilometers, to {miles:.2f} miles")
